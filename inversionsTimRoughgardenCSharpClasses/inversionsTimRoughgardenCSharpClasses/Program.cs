4f66343ffd6e0139ff1b9eeeb06ffe6265f84bbb
alexisjchughes@gmail.com <alexisjchughes@gmail.com>
2015-11-12 12:19:05 +0000

    VS2013 mainly cuda

---
+++ inversionsTimRoughgardenCSharpClasses/inversionsTimRoughgardenCSharpClasses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inversionsTimRoughgardenCSharpClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class arrayToSort
+    {
+        public int[] intA;
+        public int[] intB;
+        public int[] intC;
+        public int[] intD;
+        public int intLenA;
+        public int intLenB;
+        public int intLenC;
+        public arrayToSort(int[] A)
+        {
+            intLenA = A.Length;
+            intA = A;
+        }
+        public int divideAndConquer()
+        {
+            intLenB = intLenA / 2;
+            intB = new int[intLenB];
+            intLenC = intLenA - intLenB;
+            intC = new int[intLenC];
+
+            for(int i=0;i<intLenB;i++)
+            {
+                intB[i] = intA[i];
+            }
+            int j=0;
+            for (int i = intLenB + 1; i < intLenA; i++)
+            {
+                intC[j] = intA[i];
+                j++;
+            }
+            return 0;
+        }
+        public int recursiveSort()
+            {
+            arrayToSort arrB = new arrayToSort(intB);
+            arrayToSort arrC = new arrayToSort(intC);
+            recursiveSort arrB;
+                recursiveSort arrC;
+                return 0;
+            }
+
+
+        }
+
+    }
+}
---
+++ mergeSortClassesTR1/mergeSortClassesTR1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mergeSortClassesTR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class arrToSort
+    {
+        public int[] intA;
+        public int[] intB;
+        public int[] intC;
+
+        public ArraySort(int[] arrayToSort)
+        {
+            intA = arrayToSort;
+            if intA.Length = 1
+            
+            
+            int[] intB = new int[intA.Length / 2];
+            for (int i = 0; i < intA.Length; i++)
+            {
+                intB[i] = intA[i];
+            }
+            int[] intC = new int[intA.Length - intA.Length / 2];
+            for (int i = 0; i < intC.Length; i++)
+            {
+                intC[i] = intA[i + intA.Length / 2];
+            }
+
+            arrToSort arrA = new arrToSort(intA);
+            arrToSort arrB = new arrToSort(intB);
+
+
+        }
+    }
+}
